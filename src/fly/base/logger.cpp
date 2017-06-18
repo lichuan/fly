27c9e3deb3b521b81c61b641594bb508f6fd7e73
root <root@localhost.localdomain>
2017-06-19 06:40:28 +0800

    fix log err in multi-thread

--- src/fly/base/logger.cpp
+++ src/fly/base/logger.cpp
@@ -61,9 +61,14 @@ void Logger::init(LOG_LEVEL level, const std::string &app, const std::string &pa
             fclose(m_file);
             std::rename(m_file_full_name.c_str(), file_name);
         }
+        else
+        {
+            fclose(m_file);
+        }
     }
     
     m_file = fopen(m_file_full_name.c_str(), "ab");
+    setvbuf(m_file, NULL, _IOLBF, 1024);
 }
 
 void Logger::_log(uint32 year, uint32 month, uint32 day, const char *format, ...)
@@ -71,9 +76,7 @@ void Logger::_log(uint32 year, uint32 month, uint32 day, const char *format, ...
     if(year != m_year.load(std::memory_order_relaxed) || month != m_month.load(std::memory_order_relaxed)
        || day != m_day.load(std::memory_order_relaxed))
     {
-        m_enter_num.fetch_sub(1, std::memory_order_relaxed);
-        
-        while(m_enter_num.load(std::memory_order_relaxed) > 0)
+        while(m_enter_num.load(std::memory_order_relaxed) > 1)
         {
             std::this_thread::yield();
         };
@@ -91,6 +94,7 @@ void Logger::_log(uint32 year, uint32 month, uint32 day, const char *format, ...
             fclose(m_file);
             std::rename(m_file_full_name.c_str(), file_name);
             m_file = fopen(m_file_full_name.c_str(), "ab");
+            setvbuf(m_file, NULL, _IOLBF, 1024);
             m_year.store(year, std::memory_order_relaxed);
             m_month.store(month, std::memory_order_relaxed);
             m_day.store(day, std::memory_order_relaxed);
@@ -100,7 +104,6 @@ void Logger::_log(uint32 year, uint32 month, uint32 day, const char *format, ...
         va_start(args, format);
         vfprintf(m_file, format, args);
         va_end(args);
-        fflush(m_file);
     }
     else
     {
@@ -108,9 +111,9 @@ void Logger::_log(uint32 year, uint32 month, uint32 day, const char *format, ...
         va_start(args, format);
         vfprintf(m_file, format, args);
         va_end(args);
-        fflush(m_file);
-        m_enter_num.fetch_sub(1, std::memory_order_relaxed);
     }
+    
+    m_enter_num.fetch_sub(1, std::memory_order_relaxed);
 
     /**************** for debug ******************/
     va_list args;
