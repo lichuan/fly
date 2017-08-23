fe3c65e112012ca92198a20c4dd8bca8aff46602
lichuan <308831759@qq.com>
2017-08-23 22:15:04 +0800

    add utc in logger

--- src/fly/base/logger.hpp
+++ src/fly/base/logger.hpp
@@ -78,65 +78,65 @@ private:
 #define LOG_DEBUG(format, ...) \
     if(fly::base::Logger::instance()->_enter(fly::base::DEBUG)) \
     { \
-        char _format[1024]; \
+        char _format[256]; \
         struct timeval _tv; \
         gettimeofday(&_tv, NULL); \
         struct tm _tm; \
         localtime_r(&_tv.tv_sec, &_tm); \
-        sprintf(_format, "%d-%02d-%02d %02d:%02d:%02d.%06ld |debug| %s:%d {%s}\n", 1900 + _tm.tm_year, 1 + _tm.tm_mon, _tm.tm_mday, _tm.tm_hour, \
-                _tm.tm_min, _tm.tm_sec, _tv.tv_usec, __FILE__, __LINE__, format); \
+        sprintf(_format, "%d-%02d-%02d %02d:%02d:%02d.%06ld [utc:%lu] |debug| %s:%d {%s}\n", 1900 + _tm.tm_year, 1 + _tm.tm_mon, _tm.tm_mday, _tm.tm_hour, \
+                _tm.tm_min, _tm.tm_sec, _tv.tv_usec, _tv.tv_sec, __FILE__, __LINE__, format); \
         fly::base::Logger::instance()->_log(1900 + _tm.tm_year, 1 + _tm.tm_mon, _tm.tm_mday, _format, ##__VA_ARGS__); \
     }
 
 #define LOG_INFO(format, ...) \
     if(fly::base::Logger::instance()->_enter(fly::base::INFO)) \
     { \
-        char _format[1024]; \
+        char _format[256]; \
         struct timeval _tv; \
         gettimeofday(&_tv, NULL); \
         struct tm _tm; \
         localtime_r(&_tv.tv_sec, &_tm); \
-        sprintf(_format, "%d-%02d-%02d %02d:%02d:%02d.%06ld |info| %s:%d {%s}\n", 1900 + _tm.tm_year, 1 + _tm.tm_mon, _tm.tm_mday, _tm.tm_hour, \
-                _tm.tm_min, _tm.tm_sec, _tv.tv_usec, __FILE__, __LINE__, format); \
+        sprintf(_format, "%d-%02d-%02d %02d:%02d:%02d.%06ld [utc:%lu] |info| %s:%d {%s}\n", 1900 + _tm.tm_year, 1 + _tm.tm_mon, _tm.tm_mday, _tm.tm_hour, \
+                _tm.tm_min, _tm.tm_sec, _tv.tv_usec, _tv.tv_sec, __FILE__, __LINE__, format); \
         fly::base::Logger::instance()->_log(1900 + _tm.tm_year, 1 + _tm.tm_mon, _tm.tm_mday, _format, ##__VA_ARGS__); \
     }
 
 #define LOG_WARN(format, ...) \
     if(fly::base::Logger::instance()->_enter(fly::base::WARN)) \
     { \
-        char _format[1024]; \
+        char _format[256]; \
         struct timeval _tv; \
         gettimeofday(&_tv, NULL); \
         struct tm _tm; \
         localtime_r(&_tv.tv_sec, &_tm); \
-        sprintf(_format, "%d-%02d-%02d %02d:%02d:%02d.%06ld |warn| %s:%d {%s}\n", 1900 + _tm.tm_year, 1 + _tm.tm_mon, _tm.tm_mday, _tm.tm_hour, \
-                _tm.tm_min, _tm.tm_sec, _tv.tv_usec, __FILE__, __LINE__, format); \
+        sprintf(_format, "%d-%02d-%02d %02d:%02d:%02d.%06ld [utc:%lu] |warn| %s:%d {%s}\n", 1900 + _tm.tm_year, 1 + _tm.tm_mon, _tm.tm_mday, _tm.tm_hour, \
+                _tm.tm_min, _tm.tm_sec, _tv.tv_usec, _tv.tv_sec, __FILE__, __LINE__, format); \
         fly::base::Logger::instance()->_log(1900 + _tm.tm_year, 1 + _tm.tm_mon, _tm.tm_mday, _format, ##__VA_ARGS__); \
     }
 
 #define LOG_ERROR(format, ...) \
     if(fly::base::Logger::instance()->_enter(fly::base::ERROR)) \
     { \
-        char _format[1024]; \
+        char _format[256]; \
         struct timeval _tv; \
         gettimeofday(&_tv, NULL); \
         struct tm _tm; \
         localtime_r(&_tv.tv_sec, &_tm); \
-        sprintf(_format, "%d-%02d-%02d %02d:%02d:%02d.%06ld |error| %s:%d {%s}\n", 1900 + _tm.tm_year, 1 + _tm.tm_mon, _tm.tm_mday, _tm.tm_hour, \
-                _tm.tm_min, _tm.tm_sec, _tv.tv_usec, __FILE__, __LINE__, format); \
+        sprintf(_format, "%d-%02d-%02d %02d:%02d:%02d.%06ld [utc:%lu] |error| %s:%d {%s}\n", 1900 + _tm.tm_year, 1 + _tm.tm_mon, _tm.tm_mday, _tm.tm_hour, \
+                _tm.tm_min, _tm.tm_sec, _tv.tv_usec, _tv.tv_sec, __FILE__, __LINE__, format); \
         fly::base::Logger::instance()->_log(1900 + _tm.tm_year, 1 + _tm.tm_mon, _tm.tm_mday, _format, ##__VA_ARGS__); \
     }
 
 #define LOG_FATAL(format, ...) \
     if(fly::base::Logger::instance()->_enter(fly::base::FATAL)) \
     { \
-        char _format[1024]; \
+        char _format[256]; \
         struct timeval _tv; \
         gettimeofday(&_tv, NULL); \
         struct tm _tm; \
         localtime_r(&_tv.tv_sec, &_tm); \
-        sprintf(_format, "%d-%02d-%02d %02d:%02d:%02d.%06ld |fatal| %s:%d {%s}\n", 1900 + _tm.tm_year, 1 + _tm.tm_mon, _tm.tm_mday, _tm.tm_hour, \
-                _tm.tm_min, _tm.tm_sec, _tv.tv_usec, __FILE__, __LINE__, format); \
+        sprintf(_format, "%d-%02d-%02d %02d:%02d:%02d.%06ld [utc:%lu] |fatal| %s:%d {%s}\n", 1900 + _tm.tm_year, 1 + _tm.tm_mon, _tm.tm_mday, _tm.tm_hour, \
+                _tm.tm_min, _tm.tm_sec, _tv.tv_usec, _tv.tv_sec, __FILE__, __LINE__, format); \
         fly::base::Logger::instance()->_log(1900 + _tm.tm_year, 1 + _tm.tm_mon, _tm.tm_mday, _format, ##__VA_ARGS__); \
     }
 
